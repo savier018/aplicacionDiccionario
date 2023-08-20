f478d1a9069f6736a4069788c67f4cba4f80504c
savier018 <acosta.savier.03@gmail.com>
2023-08-20 10:24:46 -0500

    cambios en el sistema de busqueda

--- DiccionaryMan/src/main/java/TDA/ArbolTrie.java
+++ DiccionaryMan/src/main/java/TDA/ArbolTrie.java
@@ -2,6 +2,7 @@ package TDA;
 
 import com.mycompany.diccionaryman.Palabra;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ArbolTrie {
     private TrieNode root;
@@ -12,62 +13,16 @@ public class ArbolTrie {
     
     public boolean isEmpty(){
         return this.root.getChildren().isEmpty();
-    }
+    }   
     
-    public void insert(String word){
-        if (word == null || word.isEmpty()){
-            throw new UnsupportedOperationException("No se ha ingresado ninguna palabra");
-        } else if (this.root == null){
-            this.root = new TrieNode();
-        } else {
-            TrieNode current = this.root;
-            for(Character ch : word.toCharArray()){
-                current = current.getChildren().computeIfAbsent(ch, c -> new TrieNode());
-                current.setContent(current.getContent() + ch);
-            }
-            current.setIsEndOfWord(true);
-        }
-    }
-    
-    public boolean search(String word){
-        if (word == null || word.isEmpty()){
-            throw new UnsupportedOperationException("No se ha ingresado ninguna palabra");
-        } else if (this.root == null) {
-            this.root = new TrieNode();
-        } else {
-            TrieNode current = this.root;
-            for (Character ch : word.toCharArray()){
-                current = current.getChildren().get(ch);
-                if (current == null){
-                    return false;
-                }
-            }
-            return current.isEndOfWord();
-        }
-        return false;
-    }
-    
-    public TrieNode getTrieNodeWithPrefix(String prefix) {
-        TrieNode current = root;
-        for (Character ch : prefix.toCharArray()) {
-            current = current.getChildren().get(ch);
-            if (current == null) {
-                return null;
-            }
-        }
-        return current;
-    }
-    
-    public void collectWordsWithPrefix(TrieNode node, String prefix, ArrayList<Palabra> result) {
+    public void collectWordsWithPrefix(TrieNode node, String palabraActual, ArrayList<Palabra> result) {
         if (node.isEndOfWord()) {
-            Palabra palabraEncontrada = new Palabra(prefix, "");
+            Palabra palabraEncontrada = new Palabra(palabraActual, "");
             result.add(palabraEncontrada);
         }
-        for (TrieNode child : node.getChildren().values()) {
-            collectWordsWithPrefix(child, prefix + child.getContent(), result);
+        for (Map.Entry<Character, TrieNode> entry : node.getChildren().entrySet()) {
+            collectWordsWithPrefix(entry.getValue(), palabraActual + entry.getKey(), result);
         }
     }
-    
-
-    
+  
 }
